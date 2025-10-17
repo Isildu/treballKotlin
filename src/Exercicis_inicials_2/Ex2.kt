ddfa6a7eeb2601379c0241dba2f5da5025da4ad7
oriol.canellas.7e3 <oriol.canellas.7e3@itb.cat>
2025-10-17 13:41:14 +0200

    jj

--- src/Exercicis_inicials_2/Ex2.kt
+++ src/Exercicis_inicials_2/Ex2.kt
@@ -4,5 +4,5 @@ package Exercicis_inicials_2
 Escriu un programa que presenti el següent menú d’opcions relacionades amb les propietats dels triangles i que permeti resoldre les operacions indicades.
  */
 fun main() {
-    println("")
+    println("n")
 }
